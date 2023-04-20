e3ac16e3fa32c2dfc065be4ba8b6928f43747600
VincenzoBusiello <126822842+VincenzoBusiello@users.noreply.github.com>
2023-04-20 17:39:05 +0200

    Update Classification.R

--- Classification.R
+++ Classification.R
@@ -37,3 +37,36 @@ percentages
 #classe3: 21.21993 %
 #classe2: 41.44658 %
 #classe1: 37.33349 %
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+#_________________________________________________________________________________________
+
+# Grand Canyon Excercise
+
+#_________________________________________________________________________________________
+
+
+library(raster)
+
+setwd("C:/lab/")
+gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
+gc
+
+plotRGB(gc, 1,2,3, stretch="lin")
+gcc <- crop(gc, drawExtent())# La funzione crop() serve per ritagliare l'immagine.
+plotRGB(gcc, 1,2,3, stretch="lin")
+ncell(gcc)# Da come risultato il numero di celle per banda della nuova immagine
+singlenr <- getValues(gcc)# Cattura dei valori dei pixel dell'immagine
+singlenr
+kcluster <- kmeans(singlenr, centers = 3)#raggruppa i pixel basandosi su delle classi. Means significa medie. K-means significa un tot di medie. ciò serve a definire il centroide di ogni nuvola per determinare la distanza di un pixel ignoto dalle nuvole di pixel conosciuti.
+kcluster
+gccclass <- setValues(gcc[[1]], kcluster$cluster)# Riconversione dei dati numerici estrapolati dall'immagine.
+cl <- colorRampPalette(c('yellow','black','red'))(100)
+plot(gcclass, col=cl)
+# class1: conglomerates
+# class2: sandstones
+# class3: volcanic rocks
+frequencies <- freq(gccclass)# calcolo della frequenza delle classi
+total <- ncell(gccclass) #calcolo del totale di pixels
+percentages = frequencies * 100 /  total
+percentages
